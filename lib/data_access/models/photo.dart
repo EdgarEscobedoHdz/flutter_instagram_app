0c015c5e948e8cd266b0d0abf0fe4f3fd64bcf9f
Edgar Escoebdo <edgaar.escobedo@gmail.com>
2020-07-06 18:50:46 -0500

    Added User and Photo models

---
+++ lib/data_access/models/photo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Photo {
+  int id;
+  String url;
+  
+  Photo(this.id, this.url);
+
+  Image get data{
+    // This will return an image soon!
+    return Image.network("https://i.ytimg.com/vi/fq4N0hgOWzU/maxresdefault.jpg");
+  }
+
+  set data(Image image){
+    // This will upload the image to the S3 and DB!
+  }
+}
---
+++ lib/data_access/models/user.dart
@@ -0,0 +1,12 @@
+class User {
+  int id;
+  String email;
+  String nickname;
+  String password;
+  String gender;
+  DateTime bDate;
+  String phoneNumber;
+  int profilePicture;
+
+  User(this.id, this.email, this.nickname, this.password, this.gender, this.bDate, this.phoneNumber, this.profilePicture);  
+}
